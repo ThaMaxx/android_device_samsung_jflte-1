c06c93df8295f50b7e159a1df5d49f627eb3671d
Steve Kondik <shade@chemlab.org>
2013-11-03 04:32:53 -0800

    jf: Enable CRDA service
    Change-Id: I7eb46bf1c95a752616e9227d62286b1f258ef9f2

--- jf-common.mk
+++ jf-common.mk
@@ -69,6 +69,7 @@ PRODUCT_PACKAGES += \
     initlogo.rle \
     init.bt.rc \
     init.carrier.rc \
+    init.crda.rc \
     init.qcom.rc \
     init.qcom.usb.rc \
     init.target.rc \
@@ -80,7 +81,10 @@ PRODUCT_PACKAGES += Torch
 # Wifi
 PRODUCT_PACKAGES += \
     libnetcmdiface \
-    macloader
+    macloader \
+    crda \
+    regulatory.bin \
+    linville.key.pub.pem
 
 # NFC packages
 PRODUCT_PACKAGES += \
